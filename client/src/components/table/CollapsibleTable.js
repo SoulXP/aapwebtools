318a918d346675796a20b8d6c69bb4a95ab1c250
SoulXP <snowflake1194@gmail.com>
2022-02-25 14:12:09 +0200

    rebuilding CollapsibleTable to better suit app

--- client/src/components/table/CollapsibleTable.js
+++ client/src/components/table/CollapsibleTable.js
@@ -1,20 +1,6 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import './CollapsibleTable.css'
-import PropTypes from 'prop-types';
 import { API_RESULTS_KEYS } from '../../http/ApiClient.js'
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 // Memoization comparison function
 function props_equal(previous, next) {
@@ -27,63 +13,23 @@ function props_equal(previous, next) {
     return p_hash(p_value) === n_hash(n_value);
 }
 
-// Schema for table types
-Row.propTypes = {
-    row: PropTypes.any.isRequired,
-};
-
 // Row entry function
 function Row({ row }) {
-    const [open, setOpen] = useState(false);
     const keys = Object.keys(row);
     
     return (
         <React.Fragment>
-            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-                <TableCell>
-                    <IconButton
-                        aria-label="expand row"
-                        size="small"
-                        onClick={() => setOpen(!open)}
-                    >
-                    {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                    </IconButton>
-                </TableCell>
-                {
-                    keys.map((c, i) => {
-                        if (i === 0) return (<TableCell key={i} component='th' scope='row'>{row[c]}</TableCell>);
-                        if (i === keys.length - 1) return (<TableCell key={i} sx={{ width: '800px' }} >{row[c]}</TableCell>);
-                        return (<TableCell key={i} >{row[c]}</TableCell>);
-                    })
-                }
-            </TableRow>
-            {/*
-            <TableRow>
-                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                     <Collapse in={open} timeout="auto" unmountOnExit>
-                        <Box sx={{ margin: 1 }}>
-                            <Typography variant="h6" gutterBottom component="div">
-                                History
-                            </Typography>
-                            <Table size="small" aria-label="purchases">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Column 1</TableCell>
-                                        <TableCell>Column 2</TableCell>
-                                        <TableCell align="right">Column 3</TableCell>
-                                        <TableCell align="right">Column 4</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                
-                                </TableBody>
-                            </Table>
-                        </Box>
-                    </Collapse>
-                </TableCell>
-            </TableRow>
-            */}
-        </React.Fragment>
+            <tr className='table-body-row'>
+            {
+                keys.map((c, i) => {
+                    const class_name = 'table-body-row-data'
+                    if (i === 0) return (<td key={i} className={class_name}>{row[c]}</td>);
+                    if (i === keys.length - 1) return (<td key={i} className={class_name}>{row[c]}</td>);
+                    return (<td key={i} className={class_name}>{row[c]}</td>);
+                })
+            }
+            </tr>
+       </React.Fragment>
     );
 }
 
@@ -92,32 +38,31 @@ function CollapsibleTable({ tableData, prepareDataRows }) {
     const { headers, body } = tableData;
 
     return (
-        <TableContainer className='table-container' component={Paper}>
-            <Table aria-label="collapsible table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell />
-                        {
-                            headers.map((c, i) => {
-                                if (i === headers.length - 1) return (<TableCell key={i} sx={{ width: '800px' }} >{c.title}</TableCell>);
-                                return (<TableCell key={i} >{c.title}</TableCell>);
-                            })
-                        }
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                {
-                    body.map((c, i) => {
-                        const row = {};
-                        for (const k of headers) {
-                            if (k.key in c) row[k.key] = c[k.key];
-                        }
-                        return (<Row key={i} row={row}/>);
-                    })
-                }
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <div className='table-container'>
+            <table>
+                <thead>
+                    <tr>
+                    {
+                        headers.map((c, i) => {
+                            if (i === headers.length - 1) return (<td key={i}>{c.title}</td>);
+                            return (<td key={i} >{c.title}</td>);
+                        })
+                    }
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        body.map((c, i) => {
+                            const row = {};
+                            for (const k of headers) {
+                                if (k.key in c) row[k.key] = c[k.key];
+                            }
+                            return (<Row key={i} row={row}/>);
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
     );
 }
 
